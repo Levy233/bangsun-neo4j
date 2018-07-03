ecbb3ecb6b49dc42d92078a5f47a0e082d79fd8f
zhf <zhouhf@bsfit.com.cn>
2018-07-03 16:11:01 +0800

    server handler

--- community/kernel/src/main/java/org/neo4j/kernel/network/handle/ServerHandler.java
+++ community/kernel/src/main/java/org/neo4j/kernel/network/handle/ServerHandler.java
@@ -3,16 +3,23 @@ package org.neo4j.kernel.network.handle;
 /**
  * Created by Think on 2018/5/24.
  */
+
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.neo4j.helpers.HostnamePort;
 import org.neo4j.kernel.cluster.AliveSlaves;
 import org.neo4j.kernel.cluster.Slave;
+import org.neo4j.kernel.network.ChunkingChannelBuffer;
 import org.neo4j.kernel.network.ResponsePacker;
 import org.neo4j.kernel.network.message.HeartBeatMessage;
 import org.neo4j.kernel.network.message.RequestContext;
+import org.neo4j.kernel.network.message.TransactionResponse;
 import org.neo4j.kernel.network.message.TransactionStreamResponse;
 import org.neo4j.kernel.network.state.StateMachine;
 import org.neo4j.logging.Log;
@@ -20,16 +27,13 @@ import org.neo4j.logging.Log;
 @ChannelHandler.Sharable
 public class ServerHandler extends ChannelInboundHandlerAdapter {
     private Log logging;
-
     private volatile AliveSlaves aliveSlaves;
-
     //未收到信号间隔次数
     private int outTime;
-
     private StateMachine stateMachine;
     private ResponsePacker responsePacker;
 
-    public ServerHandler(AliveSlaves aliveSlaves, int outTime, StateMachine stateMachine, Log logService,ResponsePacker responsePacker){
+    public ServerHandler(AliveSlaves aliveSlaves, int outTime, StateMachine stateMachine, Log logService, ResponsePacker responsePacker) {
         this.logging = logService;
         this.outTime = outTime;
         this.aliveSlaves = aliveSlaves;
@@ -58,11 +62,11 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("Server: channelActive: "+ctx.channel().id());
+        System.out.println("Server: channelActive: " + ctx.channel().id());
         System.out.println(aliveSlaves.getSlaves().size());
         try (Slave slave = aliveSlaves.search(ctx)) {
             if (slave != null) {
-                System.out.println("Server: "+slave.getCtx().channel().remoteAddress());
+                System.out.println("Server: " + slave.getCtx().channel().remoteAddress());
             } else {
                 aliveSlaves.add(slave);
             }
@@ -72,29 +76,30 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("Server: channelInactive: "+ctx.channel().id());
+        System.out.println("Server: channelInactive: " + ctx.channel().id());
         ctx.fireChannelInactive();
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("111");
-        System.out.println("Server: channelRead "+msg.toString());
+        System.out.println("Server: channelRead " + msg.toString());
         if (msg instanceof HeartBeatMessage) {
             stateMachine.handle((HeartBeatMessage) msg);
         } else if (msg instanceof RequestContext) {
             TransactionStreamResponse response = responsePacker.packTransactionStreamResponse((RequestContext) msg);
+//            ctx.channel().writeAndFlush(new TransactionResponse(1111,new HostnamePort("127.0.0.1",9090),"prepare to apply transactions"));
             ctx.channel().writeAndFlush(response);
-        }else {
+        } else {
             ctx.fireChannelRead(msg);
         }
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("Server: channelReadComplete "+ctx.channel().id());
+        System.out.println("Server: channelReadComplete " + ctx.channel().id());
         ctx.fireChannelReadComplete();
     }
+
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         try (Slave slave = aliveSlaves.search(ctx)) {
@@ -109,5 +114,11 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 //    private Slave recognizeSlave(ChannelHandlerContext ctx,Object msg){
 //        HeartBeatMessage msg1 = (HeartBeatMessage)msg;
 //        return new Slave(ctx,msg1.getInstanceId(),msg1.getSender());
+//    }
+//    private ChannelBuffer serializeToBuffer(TransactionStreamResponse response,Channel channel){
+//        ChannelBuffer buffer = ChannelBuffers.dynamicBuffer();
+//        ChunkingChannelBuffer chunkingBuffer = new ChunkingChannelBuffer( buffer,
+//                channel, chunkSize, internalProtocolVersion, applicationProtocolVersion );
+//        return buffer;
 //    }
 }
